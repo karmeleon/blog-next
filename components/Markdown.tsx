24eda92d52b509f00dde687678f2e7574661d786
Shawn Walton <sh@wn.zone>
2021-06-28 12:01:39 -0700

    improve markdown, pull out page shell

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -3,6 +3,14 @@ import ReactMarkdown from 'react-markdown';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
 import { css } from '@emotion/react';
 import Image from 'next/image';
+import {
+	H1,
+	H2,
+	H3,
+	H4,
+	A,
+	Hr,
+} from '../shared/styles';
 //import {dark} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 interface Props {
@@ -26,15 +34,21 @@ const components = {
 			</code>
 		)
 	},
-	img({node, src}) {
+	img({node, src, ...props}) {
 		// TODO: get the next image optimzation working
 		return <img src={src} css={css`
 			display: block;
 			max-width: 100%;
 			max-height: 100%;
 			margin: 0 auto;
-		`} />;
-	}
+		`} {...props} />;
+	},
+	h1: H1,
+	h2: H2,
+	h3: H3,
+	h4: H4,
+	a: A,
+	hr: Hr,
 };
 
 const Markdown = ({ children }: Props) => <ReactMarkdown components={components}>{children}</ReactMarkdown>;
