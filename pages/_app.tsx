cab329e67c8990dab5100fb9a5d240bcd8c72ee1
Shawn Walton <sh@wn.zone>
2021-06-28 18:13:56 -0700

    add ga (untested)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { globalStyles } from '../shared/styles';
 import '../shared/headerAnimation.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			// log the pageview with their URL
+			if ('gtag' in window) {
+				(window as any).gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+					page_path: url,
+				});
+			}
+		};
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on('routeChangeComplete', handleRouteChange);
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off('routeChangeComplete', handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<>
 			{globalStyles}
